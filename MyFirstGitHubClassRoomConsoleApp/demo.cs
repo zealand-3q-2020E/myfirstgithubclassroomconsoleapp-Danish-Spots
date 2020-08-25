0e67bfc996500833fbcc85e20250a1f009899b1c
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-08-25 13:07:56 +0200

    demo class, properties and constructor added

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class demo
+    {
+        public demo()
+        {
+
+        }
+        public int Pro1 { get; set; }
+        public string Pro2 { get; set; }
+        public bool Pro3 { get; set; }
+    }
+}
